98d0d7e597004578822951186d1168f321b8cdec
seiya.ie <seiya2342@icloud.com>
2025-07-26 02:11:08 +0900

    コードを修正しました。

--- js/components/splide.js
+++ js/components/splide.js
@@ -9,14 +9,14 @@ export const initSplide = () => {
         resetProgress: false,
         focus: "center",
         fixedWidth: "320px",
-        fixedHeight: '320px',
+        fixedHeight: "320px",
         gap: "60px",
         pagination: false,
         breakpoints: {
-            1200: { gap: "48px", fixedWidth: "500px", fixedHeight: "500px"},
-            1024: { gap: "40px", fixedWidth: "460px", fixedHeight: "460px" },
-            768: { perPage: 2, gap: "32px", fixedWidth: "320px", fixedHeight: '320px'},
-            480: { perPage: 1, gap: "24px", fixedWidth: "100%", fixedHeight: "100%" },
+            1200: { gap: "48px" },
+            1024: { gap: "40px" },
+            768: { perPage: 2, gap: "32px" },
+            480: { perPage: 1, gap: "24px" },
         },
     });
     let restartTimeout;
