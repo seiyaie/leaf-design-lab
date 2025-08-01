81a293845b6e92dd20515e3eed20b626c5a7853d
seiya.ie <seiya2342@icloud.com>
2025-08-01 11:11:00 +0900

    コードを修正しました。

--- js/components/accordion.js
+++ js/components/accordion.js
@@ -2,12 +2,36 @@ export const initAccordion = () => {
     // query selectors
     const details = document.querySelectorAll(".js-accordion-details");
 
+    // Opening Keyframes
+    const openingKeyframes = (content) => {
+        return {
+            height: [0, content.scrollHeight + "px"],
+            opacity: [0, 1],
+        };
+    };
+
+    // Closing Keyframes
+    const closingKeyframes = (content) => {
+        return {
+            height: [content.scrollHeight + "px", 0],
+            opacity: [1, 0],
+        };
+    };
+
+    // Options
+    const options = {
+        duration: 300,
+        easing: "linear",
+    };
+
+    // 要素がない場合は処理を中断
     if (!details.length) return;
 
     details.forEach((el) => {
         const summary = el.querySelector(".js-accordion-summary");
         const content = el.querySelector(".js-accordion-content");
 
+        // サマリーとコンテンツがない場合は処理を中断
         if (!summary || !content) return;
 
         summary.addEventListener("click", (e) => {
@@ -18,37 +42,20 @@ export const initAccordion = () => {
                 return;
             }
 
-            const currentHeight = content.scrollHeight + "px";
-
-            // openingKeyframes
-            const openingKeyframes = {
-                height: [0, currentHeight],
-                opacity: [0, 1],
-            };
-
-            const closingKeyframes = {
-                height: [currentHeight, 0],
-                opacity: [1, 0],
-            };
-
-            const options = {
-                duration: 300,
-                easing: "linear",
-                fill: "forwards",
-            };
-
             if (el.open) {
+                // アコーディオンを閉じる処理
                 el.dataset.isAnimation = "true";
-                const closeAnimation = content.animate(closingKeyframes, options);
+                const closeAnimation = content.animate(closingKeyframes(content), options);
                 closeAnimation.onfinish = () => {
                     el.removeAttribute("open");
                     el.dataset.isAnimation = "false";
                 };
             } else {
-                el.setAttribute("open", "");
+                // アコーディオンを開く処理
+                el.setAttribute("open", "true");
                 el.dataset.isAnimation = "true";
 
-                const openAnimation = content.animate(openingKeyframes, options);
+                const openAnimation = content.animate(openingKeyframes(content), options);
                 openAnimation.onfinish = () => {
                     el.dataset.isAnimation = "false";
                 };
